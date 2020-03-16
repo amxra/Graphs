d477f7042cb92437b720cce0b60ac55c012c5a72
Amira Adediran <amira@Amiras-MBP.ipnxng.net>
2020-03-16 21:54:57 +0100

    complete add vertex, add edge, get neighbours,bft

--- projects/graph/graph.py
+++ projects/graph/graph.py
@@ -7,32 +7,60 @@ class Graph:
 
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
     def __init__(self):
-        self.vertices = {}
+        self.vertices = {} # adjacency list (dictionary) # adjacency matrix (2d list or array) 
 
     def add_vertex(self, vertex_id):
         """
         Add a vertex to the graph.
         """
-        pass  # TODO
+        # add vertex
+        self.vertices[vertex_id] = set()
 
     def add_edge(self, v1, v2):
         """
         Add a directed edge to the graph.
         """
-        pass  # TODO
+        # add edges
+
+        if v1 in self.vertices and v2 in self.vertices: # check that v1 and v2 exist in the vertices dictionary
+            self.vertices[v1].add(v2) # add v2 to the vertices at v1
+        else:
+            # raise and exception and give an error
+            raise IndexError("That vertex does not exist")   
 
     def get_neighbors(self, vertex_id):
         """
         Get all neighbors (edges) of a vertex.
         """
-        pass  # TODO
+        # TODO
+        return self.vertices[vertex_id]
 
     def bft(self, starting_vertex):
         """
         Print each vertex in breadth-first order
         beginning from starting_vertex.
         """
-        pass  # TODO
+        # TODO
+        # create empty queue enqueue the starting vertex id
+        q = Queue()
+        q.enqueue(starting_vertex)
+        # create a set to store our visited vertices
+        visited = set()
+
+        # while queue is not empty (len greater than 0)
+        while q.size() > 0:
+            # dequeue the first vertex
+            v = q.dequeue()
+            # if that vertex has not been visited
+            if v not in visited:
+                # mark as visited and print for debugging
+                visited.add(v)
+                print(v) # for debugging
+                # iterate over the child vertices of the current vertex
+                for next_vertex in self.vertices[v]:
+                    # enqueue the next vertex
+                    q.enqueue(next_vertex)
+
 
     def dft(self, starting_vertex):
         """
